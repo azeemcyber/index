04fc7c347073a47263bc5db0f74141cc10acae68
azeemcyberspace <75563954+azeemcyber@users.noreply.github.com>
2020-12-06 12:24:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+
+	</head>
+<body>
+	<form method="post" action="" enctype='multipart/form-data'>
+		<input type='file' name='file'/>
+		<input type='submit' value='upload' name='upload'>
+	</form>
+	
+	</body>
+	</html>
+	
